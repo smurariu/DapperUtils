bd8c2b54549864c9e428b2fdb0bc7ab810e840d0
Serban Murariu <Serban.Murariu@sundiogroup.com>
2017-01-11 11:01:00 +0100

    Added the DapperProviderBase

---
+++ DapperProviderBase.cs
@@ -0,0 +1,158 @@
+﻿﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Linq.Mapping;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace DapperUtils
+{
+    public class DapperProviderBase
+    {
+        private readonly string _connectionString = String.Empty;   
+
+        /// <summary>
+        /// Creates a new instance of the base provider
+        /// </summary>
+        /// <param name="connectionString">Connectoin string to use</param>
+        public DapperProviderBase(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        /// <summary>
+        /// Connects async to the database
+        /// </summary>
+        /// <typeparam name="R">The return type of the method to execute</typeparam>
+        /// <param name="f">Method to execute</param>
+        /// <returns></returns>
+        public async Task<R> ConnectAsync<R>(Func<IDbConnection, Task<R>> f)
+        {
+            ConfigureDapperColumnMapping<R>();
+
+            using (IDbConnection connection = await GetOpenConnectionAsync())
+            {
+                return await f(connection);
+            }
+        }
+
+        /// <summary>
+        ///  Convert IEnumerable<T> to DataTable 
+        ///  (useful when calling storeds that have table-valued parameters)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        public DataTable CreateTable<T>(IEnumerable<T> collection)
+        {
+            // Fetch the type of List contained in the ParamValue
+            var tableType = typeof(T);
+
+            // Create DataTable which will contain data from List<T>
+            var dataTable = new DataTable();
+
+            // Fetch the Type fields count
+            int columnCount = tableType.GetProperties().Count();
+
+            var columnNameMappingDictionary = new Dictionary<string, string>();
+
+            // Create DataTable Columns using table type field name and their types
+            // Traversing through Column Collection
+            for (int counter = 0; counter < columnCount; counter++)
+            {
+                var propertyInfo = tableType.GetProperties()[counter];
+
+                string columnName = GetColumnAttributeValue(propertyInfo);
+
+                columnNameMappingDictionary.Add(propertyInfo.Name, columnName ?? propertyInfo.Name);
+
+                dataTable.Columns.Add(columnName ?? propertyInfo.Name, tableType.GetProperties()[counter].PropertyType);
+            }
+
+            // Return parameter with null value
+            if (collection == null)
+                return dataTable;
+
+            // Traverse through number of entries / rows in the List
+            foreach (var item in collection)
+            {
+                // Create a new DataRow
+                DataRow dataRow = dataTable.NewRow();
+
+                // Traverse through type fields or column names
+                for (int counter = 0; counter < columnCount; counter++)
+                {
+                    // Fetch Column Name
+                    string columnName = columnNameMappingDictionary[tableType.GetProperties()[counter].Name];
+
+                    //Fetch Value for each column for each element in the List<T>
+                    dataRow[columnName] = item
+                        .GetType().GetProperties()[counter]
+                        .GetValue(item);
+                }
+                // Add Row to Table
+                dataTable.Rows.Add(dataRow);
+            }
+
+            return (dataTable);
+        }
+
+        #region Private Methods
+
+        private async Task<IDbConnection> GetOpenConnectionAsync()
+        {
+            var dbConnection = new SqlConnection(_connectionString);
+
+            await dbConnection.OpenAsync();
+
+            return dbConnection;
+        }
+
+        private string GetColumnAttributeValue(PropertyInfo propertyInfo)
+        {
+            ColumnAttribute columnAttribute = propertyInfo.GetCustomAttributes(false).OfType<ColumnAttribute>().FirstOrDefault();
+
+            return columnAttribute != null ? columnAttribute.Name : null;
+        }
+
+        private void ConfigureDapperColumnMapping<T>()
+        {
+            ConfigureDapperColumnMapping(typeof(T));
+        }
+
+        private void ConfigureDapperColumnMapping(Type propertyType)
+        {
+            Type[] t = { propertyType };
+
+            if (propertyType.IsGenericType)
+            {
+                t = propertyType.GetGenericArguments();
+            }
+
+            Func<Type, string, System.Reflection.PropertyInfo> mapping = (type, columnName) =>
+                type.GetProperties().FirstOrDefault(prop => GetColumnAttributeValue(prop) == columnName);
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                SqlMapper.SetTypeMap(t[i], new CustomPropertyTypeMap(t[i], mapping));
+
+                //map properties that are not value types
+                foreach (var property in t[i].GetProperties(BindingFlags.Instance
+                                                          | BindingFlags.NonPublic
+                                                          | BindingFlags.Public))
+                {
+                    if (property.PropertyType.IsValueType == false)
+                    {
+                        //WARNING: Infinite recursion is possible
+                        ConfigureDapperColumnMapping(property.PropertyType);
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,110 @@
 # DapperUtils
-Bits of code to make working with dapper more enjoyable
+
+Bits of code to make working with dapper more enjoyable.
+
+## DapperProviderBase
+
+Dapper provider base is a base class that contains a few useful methods when creating 
+Dapper-based providers. Dapper allows you to execute queries against sql server and maps 
+the results to .NET objects.  
+
+Examples below:
+
+## 1) Retrieval of a entity using a stored procedure
+
+```cs
+public class DomainSpecifficReadProvider : DapperProviderBase 
+{
+    ...
+
+    public async Task<DomainEntity> GetDomainEntityAsync(string domainEntityId)
+    {
+        string command = "EXEC GET_DOMAIN_ENTITY_BY_ID @DOMAIN_ENTITY_ID";
+        var parameters = new DynamicParameters();
+        parameters.Add("DOMAIN_ENTITY_ID", domainEntityId);
+
+        var cd = new CommandDefinition(command, parameters);
+        IEnumerable<DomainEntityReadModel> domainEntities = await ConnectAsync(c => c.QueryAsync<DomainEntityReadModel>(cd));
+        
+        ...
+    }
+    ...
+}
+```
+
+The relevant properties of this entity must be decorated with the ```ColumnAttribute``` like in the example below:
+
+```cs
+    using System.Data.Linq.Mapping;
+
+    class DomainEntityReadModel
+    {
+        [Column(Name = "DOMAIN_ENTITY_ID")]
+        public int DomainEntityId { get; set; }
+
+        [Column(Name = "DOMAIN_ENTITY_NAME")]
+        public string DomainEntityName { get; set; }
+
+        [Column(Name = "DOMAIN_ENTITY_DESCRIPTION")]
+        public string DomainEntityDescription { get; set; }
+
+        [Column(Name = "DOMAIN_ENTITY_CODE")]
+        public string DomainEntityCode { get; set; }
+    }
+
+``` 
+
+Needles to say that the names you specify here must match the column names returned by your database query.
+
+## 2) Passing table-valued parameters to a query
+
+```cs
+
+public class CustomersWriteProvider : DapperProviderBase 
+{
+    ...
+    public async Task<int> CreateCustomerAsync(Customer customer)
+    {
+        DataTable phones = CreateTable(customer.Phones.Select(c => new PhoneWriteModel { PhoneTypeId = (int)c.Type, PhoneNumber = c.Number }));
+
+        string command = "EXEC CREATE_CUSTOMER @FIRSTNAME, @LASTNAME, @EMAIL, @Phones, @CUSTOMER_ID OUTPUT";
+
+        DynamicParameters dp = new DynamicParameters();
+        dp.Add("FIRSTNAME", customer.FirstName);
+        dp.Add("LASTNAME", customer.LastName);
+        dp.Add("EMAIL", customer.Email);
+
+   x    dp.Add("Phones", phones.AsTableValuedParameter("PhonesTableType"));
+
+        dp.Add("CUSTOMER_ID", 0, DbType.Int32, ParameterDirection.Output);
+
+        var cd = new CommandDefinition(command, dp);
+
+        int affectedRows = await ConnectAsync(c => c.ExecuteAsync(cd));
+        customer.Id = dp.Get<int>("CUSTOMER_ID");
+
+        return customer.Id;
+    }
+    ...
+}
+
+```
+As you can see on the lines marked with an ```x``` the parameters are added "```AsTableValuedParameter```" and here you must pass the exact name you used in sql server to define your parameter table type.
+
+The ```PhoneWriteModel``` has to match the structure of the parameter table type you have in sql server:
+
+```cs
+    class PhoneWriteModel
+    {
+        [Column(Name = "PHONE_ID")]
+        public int PhoneId { get; set; }
+
+        [Column(Name = "PHONE_NUMBER")]
+        public string PhoneNumber { get; set; }
+
+        [Column(Name = "PHONE_TYPE_ID")]
+        public int PhoneTypeId { get; set; }
+    }
+```
+
+Happy coding.
